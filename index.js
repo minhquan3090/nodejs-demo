7a24eed5fd091fafaa8067ef026eaeeae238ff0a
Minh Quan <minhquantamky@gmail.com>
2023-08-14 22:18:10 +0700

    Nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API!");
+});
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
