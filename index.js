82ef074a29c8d1d45d5771dcf10189964e2d91e0
Minh Quan <minhquantamky@gmail.com>
2023-08-15 15:45:06 +0700

    Nodejs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'))
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API!");
@@ -14,7 +17,4 @@ app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
 
-console.log("Starting");
-
-console.log("Starting");
 
