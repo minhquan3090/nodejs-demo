6a2f1942dfa1c88fa26dc54fe71a9a19dccc63af
Minh Quan <minhquantamky@gmail.com>
2023-08-15 20:49:02 +0700

    nodeN

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
