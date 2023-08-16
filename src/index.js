175e834a222ee7eddf6f70697ee1efe70b221152
Minh Quan <minhquantamky@gmail.com>
2023-08-16 21:46:13 +0700

    node

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname,'public')));
+const route = require("./routes");
+
+// app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
 
 app.engine(
   "hbs",
@@ -17,13 +23,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
